161c1399d056ad0f857927b131134dcea8d4b290
jschwabe <mail@jschwabe.site>
2024-07-06 19:43:49 +0200

    feat(topic): basic implementation
    requires merge of changes from #48

--- src/Commands.cpp
+++ src/Commands.cpp
@@ -76,25 +76,73 @@ void Server::quit(std::string after, Client const& client) {
 	}
 }
 
+std::string get_cnl(std::string data) {
+	size_t pos = data.find_first_of("#");
+	if (pos != std::string::npos) {
+		size_t end = data.find_first_of(" :", pos);
+		if (end - pos > 1) {
+			return data.substr(pos, end - pos);
+		}
+	}
+	return "";
+}
+
+std::string get_additional(std::string data) {
+	size_t pos = data.find_first_of(" :");
+	if (pos != std::string::npos) {
+		return data.substr(pos);
+	}
+	return "";
+}
+
+void Server::topic(std::string after, Client const& client) {
+	std::string channel_name = get_cnl(after);
+	std::string new_topic
+		= get_additional(after); // @todo skip space and colon
+	if (!channel_name.empty()) {
+		std::vector<Channel>::iterator channel
+			= find_cnl(channel_name, _channels);
+		if (channel != _channels.end()) {
+			if (new_topic.empty()) {
+				// only single channel is given as argument
+				if (channel != _channels.end()) {
+					if (channel->_topic.empty()) {
+						client.Output(RPL_NOTOPIC);
+					} else {
+						client.Output(RPL_TOPIC);
+					}
+				}
+			} else {
+				// @todo check if user is operator of the channel
+				// if so, allow it to change the topic
+				new_topic[0] == ' '
+					? new_topic = new_topic.substr(1)
+					: new_topic;
+				new_topic[0] == ':'
+					? new_topic = new_topic.substr(1)
+					: new_topic;
+				channel->_topic = new_topic;
+			}
+		}
+	}
+}
+
 void Server::executeCommand(Client const&      client,
 							std::string const& data) {
 	std::string cmd   = get_cmd(data);
 	std::string after = get_after_cmd(data);
 	if (data == "QUIT" || cmd == "QUIT") {
-		// message needs to be broadcastest to the whole channel that he is in.
-		// when no message, then just display <name> client quits to the channel
-		// @todo output to channels
 		quit(after, client);
-	}
-	if (!after.empty()) {
+	} else if (!after.empty()) {
 		if (cmd == "JOIN" && after[0] == '#') {
 			join(after.substr(0, after.find_first_of("\r\n")),
 				 client);
-		}
-		if (cmd == "PRIVMSG") {
-			if (!after.empty()) {
-				privmsg(after, client);
-			}
+		} else if (cmd == "PRIVMSG") {
+			privmsg(after, client);
+		} else if (cmd == "TOPIC" && after[0] == '#') {
+			topic(after.substr(0, after.find_first_of("\r\n")),
+				  client);
+			// @follow-up do this in get_after_cmd?
 		}
 	}
 }
