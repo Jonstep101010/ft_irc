ff5d11b5ddaf7aeb76607234cdae2e6e74a7601b
jschwabe <mail@jschwabe.site>
2024-07-07 21:31:31 +0200

    feat(topic): refactor & enable topic deletion & change format config
    simplified logic, enable short statements (mainly for return)

--- src/Commands.cpp
+++ src/Commands.cpp
@@ -71,43 +71,39 @@ void Server::quit(std::string after, Client const& client) {
 }
 
 void Server::topic(std::string after, Client const& client) {
-	std::string channel_name = get_cnl(after);
-	std::string new_topic    = get_additional(after);
-	if (!channel_name.empty()) {
-		std::vector<Channel>::iterator channel
-			= find_cnl(channel_name, _channels);
+	const std::string              channel_name = get_cnl(after);
+	std::vector<Channel>::iterator channel
+		= find_cnl(channel_name, _channels);
+	if (channel == _channels.end()) { return; }
+	std::string new_topic = get_additional(after);
+	if (new_topic.empty()
+		&& after[after.find_first_of(" ") + 1] != ':') {
+		// only single channel is given as argument, possible postfix of space
 		if (channel != _channels.end()) {
-			if (new_topic.empty()) {// @follow-up need to be able to delete topic
-				// only single channel is given as argument
-				if (channel != _channels.end()) {
-					if (channel->_topic.empty()) {
-						client.Output(RPL_NOTOPIC);
-					} else {
-						client.Output(RPL_TOPIC);
-					}
-				}
+			if (channel->_topic.empty()) {
+				client.Output(RPL_NOTOPIC);
 			} else {
-				// @todo check if user is operator of the channel
-				// if so, allow it to change the topic
-				if (!channel->_topic_protection) {
-					// allow anyone to change the topic
+				client.Output(RPL_TOPIC);
+			}
+		}
+	} else {
+		// @todo check if user is operator of the channel
+		// if so, allow it to change the topic
+		if (!channel->_topic_protection) {
+			// allow anyone to change the topic
+			channel->setTopic(new_topic);
+		} else {
+			// @follow-up simplify this
+			std::vector<Client>::iterator client_it
+				= std::find(channel->_clients.begin(),
+							channel->_clients.end(), client);
+			if (client_it != channel->_clients.end()) {
+				long client_idx = distance(
+					channel->_clients.begin(), client_it);
+				if (channel->_is_operator[client_idx]) {
 					channel->setTopic(new_topic);
 				} else {
-					// @follow-up simplify this
-					std::vector<Client>::iterator client_it
-						= std::find(channel->_clients.begin(),
-									channel->_clients.end(),
-									client);
-					if (client_it != channel->_clients.end()) {
-						long client_idx
-							= distance(channel->_clients.begin(),
-									   client_it);
-						if (channel->_is_operator[client_idx]) {
-							channel->setTopic(new_topic);
-						} else {
-							client.Output(ERR_CHANOPRIVSNEEDED);
-						}
-					}
+					client.Output(ERR_CHANOPRIVSNEEDED);
 				}
 			}
 		}
